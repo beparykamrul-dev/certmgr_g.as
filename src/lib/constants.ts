e3425204700f0b6777eed683b6ba691d7b3e0c1b
Kamrul islam <227916250+beparykamrul-dev@users.noreply.github.com>
2026-09-09 16:06:39 -0600

    feat: centralize FTN runtime policy constants

---
+++ src/lib/constants.ts
@@ -0,0 +1,4 @@
+export const FTN_SERVICE_NAME = 'ftn-cert-control';
+export const API_BASE = '/api';
+export const SYNTHETIC_DATA_ALLOWED = false;
+export const PRIVILEGED_ACTION_POLICY = 'approval_required';
